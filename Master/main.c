0bb6f03dce945a519230b30301aea43192ed822d
Aydin Simsek <43919074+aydinsimsek@users.noreply.github.com>
2020-11-09 13:16:26 +0300

    sends data

--- Master/main.c
+++ Master/main.c
@@ -11,7 +11,7 @@ void SPI_Transmit(char c)
 	while((SSI0->SR & 0x2) == 0x0); // wait until transmit FIFO is not full   
 	SSI0->DR = c; // send the data 
 	while((SSI0->SR & 0x10) == 0x10); //  wait until busy bit becomes 0 
-  GPIOA->DATA |= 0x8; // Deselect the slave   
+  	GPIOA->DATA |= 0x8; // Deselect the slave   
 }
 
 void delay()
@@ -22,8 +22,8 @@ void delay()
 
 int main()  
 {
-	SYSCTL->RCGCSSI = 0x1; // Enable and provide a clock to SPI0
- 	SYSCTL->RCGCGPIO = 0x1; // Enable and provide a clock to GPIO PortA 
+	SYSCTL->RCGCSSI |= 0x1; // Enable and provide a clock to SPI0
+ 	SYSCTL->RCGCGPIO |= 0x1; // Enable and provide a clock to GPIO PortA 
 	
 	GPIOA->AFSEL |= 0x3C; // Enable alternate functions on PA2, PA3, PA4, PA5  
 	GPIOA->PCTL |= 0x222200; // Assign SPI signals to PA2, PA3, PA4, PA5   
@@ -35,7 +35,7 @@ int main()
 	SSI0->CC = 0x0; // Select the SPI Baud Clock Source as system clock 
 	SSI0->CPSR = 0x10; // Set the clock frequency to 1 MHz 
 	SSI0->CR0 = 0x7; // Freescale SPI mode, 1 Mbps bit rate, 8 data bits 
-  SSI0->CR1 |= 0x2; // Enable SPI    
+  	SSI0->CR1 |= 0x2; // Enable SPI    
 	
 	char red = 'R';
 	char green = 'G'; 
